a362aa925a6ce20855bf84f1301be6e4982ab782
Hiral831 <hiralborad831@gmail.com>
2024-09-27 15:00:54 +0530

    register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,16 +36,24 @@ const registerUser = asyncHandler(async (req,res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path
-  const coverImageLocalPath = req.files?.coverImage[0].path
+  // const coverImageLocalPath = req.files?.coverImage[0].path
 
   console.log("----> req.files: ",req.files)
+
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImageLocalPath) 
+  && req.files.coverImageLocalPath.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+  } 
+  
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required")
+  }
+
 
   const avatar = await uploadOnCloudinary(avatarLocalPath)
   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-  if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar file is required")
-  }
 
   const user = await User.create({
     fullName,
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -12,4 +12,7 @@ const storage = multer.diskStorage({
   
 export const upload = multer({ 
     storage, 
+    limits: {
+      fileSize: 1024 * 1024 * 5 // Limit file size to 5MB (optional)
+    },
 })
