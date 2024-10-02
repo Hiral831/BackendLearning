94283453277e8eda17416f2c7f3a36c9f285615d
Hiral831 <hiralborad831@gmail.com>
2024-10-02 16:17:39 +0530

    add controllers using aggregation user.controller.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
-import { logoutUser, loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {  changeCurrentPassword, 
+          getUserChannelProfile, 
+          getWatchHistory, 
+          logoutUser, 
+          loginUser, 
+          registerUser, 
+          refreshAccessToken, 
+          updateAccountDetails, 
+          updateUserAvatar, 
+          updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,5 +32,14 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)  //taking input from params check controller to know more 
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
